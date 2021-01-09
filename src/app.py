7e659127d94f7aa8b1e1124a1241c5caf1d214ea
ishibashiryota <ryouta1209@live.jp>
2021-01-10 02:51:02 +0900

    circular import解決

--- src/app.py
+++ src/app.py
@@ -2,15 +2,15 @@ import uuid
 
 from flask import Flask
 
-from blueprints.auth import auth_bp
-from blueprints.home import home_bp
-
 
 def create_app():
 	app = Flask(__name__)
 	app.secret_key = uuid.uuid4().hex
 
 	# register blueprints
+	from blueprints.auth import auth_bp
+	from blueprints.home import home_bp
+
 	app.register_blueprint(home_bp)
 	app.register_blueprint(auth_bp)
 
