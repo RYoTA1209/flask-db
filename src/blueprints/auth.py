8095118a044059040ce05e35e51d40d0a94e9a55
ishibashiryota <ryouta1209@live.jp>
2021-01-20 16:04:24 +0900

    [feature/complete]サインアップ/サインイン処理の実装

--- src/blueprints/auth.py
+++ src/blueprints/auth.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, url_for, render_template, request
 
+from database import db
 from forms.SignupForm import SignupForm
 from forms.SigninForm import SigninForm
+from models.Users import User
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth/')
 
@@ -18,6 +20,19 @@ def sign_in():
 		if form.validate_on_submit():
 			# サインインの処理
 			print(form.email.data, form.password.data)
+			email = form.email.data
+			password = form.password.data
+
+			# emailが登録済みかどうかチェック！
+			if not db.session.query(User).filter(User.email == email).first():
+				return render_template('signup.html', form=form, title='ログイン', endpoints='auth.sign_in')
+
+			user = db.session.query(User).filter(User.email == email).first()
+
+			# パスワードのチェック！
+			if not user.password == password:
+				return render_template('signup.html', form=form, title='ログイン', endpoints='auth.sign_in')
+
 			return redirect(url_for('home.home'))
 		else:
 			print('Invalid input', form.email.errors, form.password.errors, form.submit.errors)
@@ -31,7 +46,19 @@ def sign_up():
 		if form.validate_on_submit():
 			# サインサップの処理
 			print(form.email.data, form.password.data)
+			email = form.email.data
+			password = form.password.data
+
+			# すでにemailが登録されている時は、DBに保存されない
+			if db.session.query(User).filter(User.email == email).first():
+				return render_template('signup.html', form=form, title='会員登録', endpoints='auth.sign_up')
+
+			# 新規にユーザーを追加する
+			new_user = User(email=email, password=password)
+			db.session.add(new_user)
+			db.session.commit()
+
 			return redirect(url_for('home.home'))
 		else:
 			print('Invalid input', form.email.errors, form.password.errors, form.submit.errors)
-	return render_template('signup.html', form=form, title='会員登録', endpoints='auth.sign_in')
+	return render_template('signup.html', form=form, title='会員登録', endpoints='auth.sign_up')
